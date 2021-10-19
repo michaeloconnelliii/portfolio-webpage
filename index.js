e609500cae6065f73606bd938c2cc72f588f33c5
Michael O'Connell <michaeloconnell@Ediths-MacBook-Air.local>
2021-10-19 10:34:20 -0700

    appropriate section is selected as user scrolls.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ window.addEventListener('click', (event) => {
 
    Header doesn't go away when nav is open.
    
-   Keep the header directly after a user clicks an intradocument link in the nav. */
+   Keep the header visible directly after a user clicks an intradocument link in the nav. */
 const headerToggle = document.querySelector('header');
 let oldScrollVal = 0;
 
@@ -60,6 +60,76 @@ window.addEventListener('hashchange', function(event) {
 // Set the offset when entering page with hash present in the url
 window.setTimeout(offsetAnchor, 0);
 
+/* Select Header link while user is in the corresponding section. 
+   For example, if user scrolls to the 'About' section, the 'About' header
+   link should be selected (it should be --primary-green). */
+
+// 1. Create an object that maps nav links to their corresponding section ID
+const navLinks = document.getElementsByClassName('nav-link');
+let sectionToObjects = {};
+
+for(let link of navLinks) {
+    let sectionId = link.getAttribute('href');
+
+    if(sectionId !== '#') {
+        sectionId = sectionId.substring(1);
+        sectionToObjects[sectionId] = [document.getElementById(sectionId), link];
+    }
+}
+
+/* 2. Use the object created in (1) to determine when we're in a specific secion and
+      select the nav link if we are. */
+
+// beginningSectionY is used s.t. we don't have the about section selected before we actually get to it scrolling
+const beginningSection = sectionToObjects['about'][0];
+const endingLink = sectionToObjects['contact'][1];
+
+window.addEventListener('scroll', function(event) {
+    
+    // beginning of the about section
+    const beginningSectionY = beginningSection.getBoundingClientRect().top;
+
+    
+    // go a little beyond the bottom of the viewport so you see the entire section header
+    let offset = 50;
+    
+    // document y position relative to the top of the viewport + the viewport size which gives the document y position 
+    // relative to the bottom of the viewport
+    let bottomScrollValue = this.window.scrollY + this.window.innerHeight;
+    
+    // used so that as soon as we find we're between a section element, we don't keep looking for which link to select
+    let linkSelected = false;
+
+    // don't select anything yet if we haven't reached the about section
+    if(bottomScrollValue > beginningSectionY + document.documentElement.scrollTop + offset) {
+        for(const sectionId in sectionToObjects) {
+            const section = sectionToObjects[sectionId][0];
+            const link = sectionToObjects[sectionId][1];
+
+            // bottom of viewport is in between section element
+            if((bottomScrollValue > section.getBoundingClientRect().top + document.documentElement.scrollTop) && 
+                (bottomScrollValue < section.getBoundingClientRect().bottom + document.documentElement.scrollTop + offset) && 
+                !linkSelected) {
+                link.classList.add('section-selected');
+                linkSelected = true;
+            }
+            else {
+                link.classList.remove('section-selected');
+            }
+        }
+
+        // reached the end of the contact section and we go beyond the section's bottom, keep contact selected
+        if(!linkSelected) {
+            endingLink.classList.add('section-selected');
+        }
+    }
+    else {
+        for(const sectionId in sectionToObjects) {
+            const link = sectionToObjects[sectionId][1];
+            link.classList.remove('section-selected');
+        }
+    }
+});
 
 /* ======= Work and Experience Modal Changer ======= */
 const uoButton = document.getElementById('uo');
